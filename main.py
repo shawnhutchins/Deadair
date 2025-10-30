b0d18d9eeeaa96262237f55e216b6b1d5a8925ba
Shawn <shawnhutchins1@gmail.com>
2025-10-29 20:34:42 -0400

    Added file extension checking

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import ffmpeg
 import os
 
-#add filtering of file types.
 #make into command line app and accept input of directory paths
 #make into tkinter app with folder selection
 
-def remove_dead_air(input_mp3, output_mp3, silence_threshold=-30, min_silence_duration=0.5):
+def remove_dead_air(input_file, output_file, silence_threshold=-30, min_silence_duration=0.5):
     """
-    Removes dead air from an MP3 file using ffmpeg-python.
+    Removes dead air from a file using ffmpeg-python.
 
     Args:
-        input_mp3 (str): Path to the input MP3 file.
-        output_mp3 (str): Path for the output MP3 file with silence removed.
+        input_file (str): Path to the input file.
+        output_file (str): Path for the output file with silence removed.
         silence_threshold (int): The volume level in dB below which audio is considered silence.
                                  Default is -30 dB.
         min_silence_duration (float): The minimum duration of silence (in seconds) to be removed.
@@ -20,26 +19,29 @@ def remove_dead_air(input_mp3, output_mp3, silence_threshold=-30, min_silence_du
     try:
         (
             ffmpeg
-            .input(input_mp3)
+            .input(input_file)
             .filter('silenceremove',
                     start_periods='0',  # Remove silence at the beginning
                     stop_periods='-1',  # Remove silence throughout the file
                     stop_duration=str(min_silence_duration),
                     detection='peak',
                     stop_threshold=str(silence_threshold) + 'dB')
-            .output(output_mp3)
+            .output(output_file)
             .run(overwrite_output=True)
         )
-        print(f"Dead air removed successfully. Output saved to: {output_mp3}")
+        print(f"Dead air removed successfully. Output saved to: {output_file}")
     except ffmpeg.Error as e:
         print(f"Error removing dead air: {e.stderr.decode()}")
 
-# Edit here to change input and output paths
-directory_path = "S:/audio_recordings/"
-output = "S:/silence_removed/"
+# Edit here to change input and output paths and set the targeted file extension
+directory_path = "input/"
+output = "output/"
+target_file_extension = ".m4a"
 
 try:
     for index, filename in enumerate(os.listdir(directory_path)):
-        remove_dead_air(os.path.join(directory_path, filename), os.path.join(output, filename))
+        f_name, f_extension = os.path.splitext(filename)
+        if f_extension == target_file_extension:
+            remove_dead_air(os.path.join(directory_path, filename), os.path.join(output, filename))
 except OSError as e:
     print(f"OS Error")
