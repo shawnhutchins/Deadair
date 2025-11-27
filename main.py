ecc0be4c36a333849057a3bd283ffae37b578a48
Shawn <shawnhutchins1@gmail.com>
2025-11-26 21:19:36 -0500

    Removed comments for completed tasks

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ import os
 
 #check for mixed forward and backslashes in path
 #add indication of success or failure
-#add indication of progress
 #add something small to the output filename, first character for sorting
 #put tooltips on the sliders to show the value of the slider
 #disable widgets while processing files
-#make function to set the progress bar at script start (set range)
 #make a button to abort the process
-#add label for progress bar showing files processed out of max files 0/0
 
 #Tries to run a ffmpeg filter silence remove on an input file
 def remove_dead_air(input_file, output_file, silence_threshold=-30, min_silence_duration=0.5):
