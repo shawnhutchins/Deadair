83538d26f2b2ecc901a7ac39e892782101afd832
Shawn <shawnhutchins1@gmail.com>
2025-12-26 23:41:30 -0500

    Added task comment

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import sys
 import os
 
 # -----TASKS-----
-#when there are no files available to process with the chosen entries, print a msg
+#get the initial window size after accounting for widget sizing for tab update
 #make the window fit to the content rather than using a fixed value
+#when there are no files available to process with the chosen entries, print a msg
 #add msg count in the text of the console tab, color coded for errors etc
 #make progress bar red when there is an error with FFMPEG or another exception
 #style console print commands like make success msg green
